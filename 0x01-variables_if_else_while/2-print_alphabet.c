91a4ef32b6a1a44b0c4efad8e286fd0ae29445ac
kaybest190 <kaybest190@gmail.com>
2022-09-09 14:10:55 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
