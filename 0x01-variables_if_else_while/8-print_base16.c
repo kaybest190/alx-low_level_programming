512372207d29c20e0ac96c426b34a976b8ad6119
kaybest190 <kaybest190@gmail.com>
2022-09-09 15:43:55 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stadio.h>
+
+/**
+ * main - prints in hexa
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10: num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; leter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
