caaa576a5bc2d8e87fca3af7f29ba02ec0e0e222
kaybest190 <kaybest190@gmail.com>
2022-09-13 14:58:29 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,10 +12,10 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (n = n; n <= 97; n++)
-		scanf("%d, ", n);
+		printf("%d, ", n);
 	}
 	else
 		for (n = n; n > 98; n--)
-			scanf("%d, ", n);
-	scanf("98\n");
+			printf("%d, ", n);
+	printf("98\n");
 }
